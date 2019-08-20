e5f7b49a9e845fab7bd841f559a8186bb438c64a
gui <deroguerre@gmail.com>
2019-08-20 21:56:56 +0200

    Initial commit

---
+++ Assets/Scripts/MeshFader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshFader : MonoBehaviour
+{
+	private bool fadeOut = false;
+
+	void Update() {
+		if (fadeOut) return;
+
+		// wait until rigibody is spleeping
+		if (GetComponent<Rigidbody>().IsSleeping()) {
+			fadeOut = true;
+			StartCoroutine(FadeOut());
+		}
+	}
+
+	IEnumerator FadeOut() {
+		float fadeTime = 2.0f;
+		var rend = GetComponent<Renderer>();
+
+		var startColor = Color.white;
+		var endColor = new Color(1, 1, 1, 0);
+
+		for (float t = 0.0f; t < fadeTime; t += Time.deltaTime) {
+			rend.material.color = Color.Lerp(startColor, endColor, t / fadeTime);
+			yield return null;
+		}
+		Destroy(gameObject);
+	}
+}
