111db2dee45e1150200bb6773808238492957d55
gui <deroguerre@gmail.com>
2019-08-31 04:34:05 +0200

    function increase player size

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
 	public bool isCollectable = false;
+	public bool isCollected = false;
 	public float resistance = 1;
 	public float weight = 1;
+
+	private Vector3 _scale;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +20,9 @@ public class Collectable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		//if(isCollected) {
+		//	transform.localScale = new Vector3(1,1,1);
+		//}
     }
 
 	public void enable(float pResistance, float pWeight) {
@@ -26,7 +31,8 @@ public class Collectable : MonoBehaviour
 	}
 
 	private IEnumerator enableCollectable(float pResistance, float pWeight) {
-		yield return new WaitForSeconds(3);
+		yield return new WaitForSeconds(2);
+		gameObject.tag = "Collectable";
 		isCollectable = true;
 		resistance = pResistance;
 		weight = pWeight;
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,24 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
 	public float increaseSizeSpeed = 0.05f;
 	public float power = 1;
 	public float weight = 3;
 
+	private Vector3 _scale = new Vector3(1, 1, 1);
+	private Vector3 _initialScale = new Vector3(4, 4, 4);
+
+	List<GameObject> playerChilds = new List<GameObject>();
+
 
 	// Start is called before the first frame update
-	void Start()
-    {
-        
-    }
+	void Start() {
+		//_scale = new Vector3(scale, scale, scale);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update() {
+		//if (transform.localScale != _scale) {
+		//	transform.localScale = Vector3.Lerp(transform.localScale, _scale, 2.0f * Time.deltaTime);
+		//}
+
+	}
 
 	private void OnCollisionEnter(Collision other) {
 
@@ -38,18 +43,50 @@ public class Player : MonoBehaviour
 				other.collider.isTrigger = true;
 				Destroy(other.rigidbody);
 
-				other.gameObject.GetComponent<ShatterToolkit.Helpers.HierarchyHandler>().enabled = false;
-				other.gameObject.GetComponent<ShatterToolkit.Helpers.ShatterOnCollision>().enabled = false;
-				other.gameObject.GetComponent<ShatterToolkit.TargetUvMapper>().enabled = false;
-				other.gameObject.GetComponent<ShatterToolkit.ShatterTool>().enabled = false;
-				other.gameObject.GetComponent<MeshCollider>().enabled = false;
+				//List<Component> componentsList = new List<Component>();
+				//componentsList.Add(gameObject.GetComponent<ShatterToolkit.Helpers.HierarchyHandler>());
+				//componentsList.Add(gameObject.GetComponent<ShatterToolkit.Helpers.ShatterOnCollision>());
+				//componentsList.Add(gameObject.GetComponent<ShatterToolkit.TargetUvMapper>());
+				//componentsList.Add(gameObject.GetComponent<ShatterToolkit.ShatterTool>());
+				//componentsList.Add(gameObject.GetComponent< MeshCollider>());
+
+				if (other.gameObject.GetComponent<ShatterToolkit.Helpers.HierarchyHandler>() != null)
+					other.gameObject.GetComponent<ShatterToolkit.Helpers.HierarchyHandler>().enabled = false;
+
+				if (other.gameObject.GetComponent<ShatterToolkit.Helpers.ShatterOnCollision>() != null)
+					other.gameObject.GetComponent<ShatterToolkit.Helpers.ShatterOnCollision>().enabled = false;
+
+				if (other.gameObject.GetComponent<ShatterToolkit.TargetUvMapper>() != null)
+					other.gameObject.GetComponent<ShatterToolkit.TargetUvMapper>().enabled = false;
+
+				if (other.gameObject.GetComponent<ShatterToolkit.ShatterTool>() != null)
+					other.gameObject.GetComponent<ShatterToolkit.ShatterTool>().enabled = false;
+
+				if (other.gameObject.GetComponent<MeshCollider>() != null)
+					other.gameObject.GetComponent<MeshCollider>().enabled = false;
 
 				//remove trigger on collider
 				//StartCoroutine(SetTrigger(collision.gameObject));
 
-				//other.gameObject.transform.SetParent(gameObject.transform);
-				other.gameObject.GetComponent<Transform>().SetParent(gameObject.GetComponent<Transform>());
+				other.gameObject.GetComponent<Collectable>().isCollected = true;
+				//other.gameObject.GetComponent<Transform>().SetParent(gameObject.GetComponent<Transform>());
+				playerChilds.Add(other.gameObject);
 
+				//_scale.x += 0.01f;
+				//_scale.y += 0.01f;
+				//_scale.z += 0.01f;
+				_scale *= 1.01f;
+
+				foreach(GameObject child in playerChilds) {
+					child.transform.parent = null;
+				}
+
+				gameObject.transform.localScale = _scale;
+				//transform.localScale = Vector3.Lerp(transform.localScale, _scale, 2.0f * Time.deltaTime);
+
+				foreach (GameObject child in playerChilds) {
+					child.transform.parent = gameObject.transform;
+				}
 
 			}
 
