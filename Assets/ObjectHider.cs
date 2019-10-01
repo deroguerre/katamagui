fc0feba2656982e68b63a82ad2d30b698fe73542
Guillaume <guillaume.deroguerre@smile.fr>
2019-10-01 14:16:00 +0200

    hidding object on raycast wip

--- Assets/ObjectHider.cs
+++ Assets/ObjectHider.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ObjectHider : MonoBehaviour
 {
 
 	private GameObject mainCamera;
-	private RaycastHit oldHit;
+	private List<GameObject> oldHits = new List<GameObject>();
+
+	// private List<RaycastHit> OLLLDHits = new List<RaycastHit>();
 
 	// Start is called before the first frame update
 	void Start()
@@ -24,15 +27,47 @@ public class ObjectHider : MonoBehaviour
 
 		Debug.DrawRay(transform.position, direction * -1, Color.red);
 
-		hits = Physics.RaycastAll(transform.position, direction * -1);
+		hits = Physics.RaycastAll(transform.position, direction * -1, distance);
 
-		Debug.Log("nb hits = " + hits.Length);
+		// Debug.Log("nb hits = " + hits.Length);
 
+		//set transparency on hit
 		foreach(RaycastHit hit in hits) {
 
-			if(!hit.transform.gameObject.GetComponent<Collectable>().isHidden) {
-				StartCoroutine(setMaterialTransparent(hit.transform.gameObject));
-				hit.transform.gameObject.GetComponent<Collectable>().isHidden = true;
+			if(hit.transform.gameObject.GetComponent<Collectable>() != null) {
+				if(!hit.transform.gameObject.GetComponent<Collectable>().isHidden) {
+
+					StartCoroutine(setMaterialTransparent(hit.transform.gameObject));
+					hit.transform.gameObject.GetComponent<Collectable>().isHidden = true;
+					oldHits.Add(hit.transform.gameObject);
+
+				}
+			}
+		}
+
+		for(int i = 0; i < oldHits.Count(); i++) {
+
+			bool isHitten = false;
+			for(int j = 0; j < hits.Count(); j++) {
+
+				if(hits[j].transform.gameObject == oldHits[i]) {
+					isHitten = true;
+				}
+			}
+
+			if(!isHitten) {
+
+				if(oldHits[i] == null) {
+					oldHits.Remove(oldHits[i]);
+				} else {
+					setMaterialOpaque(oldHits[i]);
+					if(oldHits[i].GetComponent<Collectable>() != null) {
+						oldHits[i].GetComponent<Collectable>().isHidden = false;
+					}
+					oldHits.Remove(oldHits[i]);
+					isHitten = false;
+					Debug.Log("MAKE OPAQUE !");
+				}
 			}
 		}
 
@@ -58,7 +93,12 @@ public class ObjectHider : MonoBehaviour
 
 	}
 
-	private void setMaterialOpaque() {
+	private void setMaterialOpaque(GameObject go) {
 
+		var rend = go.GetComponent<Renderer>();
+
+		//set material to opaque
+		StandardShaderUtils.ChangeRenderMode(rend.material, StandardShaderUtils.BlendMode.Opaque);
+		rend.material.color = Color.white;
 	}
 }
